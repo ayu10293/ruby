1b378be8aaff18246ee7885026277b9a276d2367
ayu <133560587+ayu10293@users.noreply.github.com@users.noreply.github.com>
2023-05-25 23:42:31 +0000

    [finish]ruby8 FizzBuzz解答見たがわからない

---
+++ greeting.rb
@@ -0,0 +1,12 @@
+def greeting(name)
+  "Hello, #{name}!"
+end
+
+puts greeting('John')
+
+def greeting(name)
+  return "Hello, #{name}!"
+  "Good morning, #{name}!"
+end
+
+puts greeting('onyankopon')
